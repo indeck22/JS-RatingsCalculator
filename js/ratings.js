b8a5f78fffd87aa9a7d3c98fad183c0a98c9a02a
Rodrigo Pinto <rgtpinto97@gmail.com>
2020-03-23 22:22:41 +0000

    Fixed logic. Completed last part

--- js/ratings.js
+++ js/ratings.js
@@ -1,21 +1,22 @@
 function collect_ratings(){
-    var ratings = { count = 0, sum = 0, average = 0 }; 
+    var ratings = { 'count' : 0, 'sum' : 0, 'average' : 0 }; 
     let rating = 0
 
-    var elements = document.querySelectorAll(".rating")
+    const elements = document.querySelectorAll(".rating")
 
-    elements.forEach(element => { 
-        ratings.count++;
-        rating += parseInt(element.id.replace("star",""))
-        ratings.sum += parseInt(element.value())
+    elements.forEach(element => {   
+        rating = parseInt(element.id.replace("star",""))
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating
     });
 
-    if(ratings.count != 0){
+    if(ratings.count !== 0){
         ratings.average = ratings.sum/ratings.count;
     }
     return ratings;
 }
 
-document.addEventListener("change", function(){
+document.addEventListener("change", ()=>{
     const ratings = collect_ratings();
+    document.querySelector("#average").value = ratings.average.toFixed(2)
 })
